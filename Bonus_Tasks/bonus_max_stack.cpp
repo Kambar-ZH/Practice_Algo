7fb48d03289cd8e474794ed54db518665e821de7
Kambar-ZH <62712679+Kambar-ZH@users.noreply.github.com>
2021-10-05 02:24:30 +0600

    Update bonus_max_stack.cpp

--- Bonus_Tasks/bonus_max_stack.cpp
+++ Bonus_Tasks/bonus_max_stack.cpp
@@ -1,32 +1,10 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
 
-#define pb(i)               push_back(i)
 #define For(i, n)           for(int i = 0; i < n; ++i)
-#define qFor(i, x, n)       for(int i = x; i < n; ++i)
-#define rFor(i, n)          for(int i = n - 1; i >= 0; --i)
 #define see                 cout << endl;
-#define all(x)              (x).begin(),(x).end()
-#define rall(x)             (x).rbegin(),(x).rend()
-#define ls x + x
-#define rs x + x + 1
-
-#define ld                  long double
-#define pii                 pair<int, int>
 #define vt                  vector
-#define ll                  long long
-#define ordered_set         tree < pair<pair<int, int>, int>,  null_type ,  less<pair<pair<int, int>, int> > ,  rb_tree_tag ,  tree_order_statistics_node_update >
-
 const int MAX = 1e9;
-const int MOD = 1e9 + 7;
-const int BINT = 32;
-const int BLONG = 64;
-const ll  INF = 1e18;
-const ld  PI  = 3.14159265358979323846;
- 
 template<typename T>
 void read(vt<T> & a) {
 	For(i, a.size()) cin >> a[i];
@@ -49,7 +27,7 @@ ll stupid(vt<int> & a) {
 	return res;
 }
 
-void write()
+int main()
 {
 	int n; cin >> n;
 	vt<int> a(n); read(a);
@@ -95,18 +73,5 @@ void write()
 	}
 	cout << "smart O(N) solution constraints up to 1e7  : " << mx << endl;
 	cout << "stupid O(N ^ 3) solution up to 1e2         : " << stupid(a) << endl;
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#ifdef DEBUG
-	freopen("out.txt", "w", stdout);
-	freopen("input.txt", "r", stdin);
-#endif
-	int T;
-	T = 1;
-	// cin >> T;
-	For(t, T) write();
 	return 0;
 }
