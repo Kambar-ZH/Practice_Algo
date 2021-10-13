08f2725ab1d061ac2f453fb6ba4915be3ddf74e2
Kambar-ZH <62712679+Kambar-ZH@users.noreply.github.com>
2021-10-13 21:38:18 +0600

    Update heap.cpp

--- Week7_Monday/heap.cpp
+++ Week7_Monday/heap.cpp
@@ -12,46 +12,58 @@ const int MOD = 1e9 + 7;
 struct Heap {
 	vt<int> heap;
 
+    int get_left_child(int node) {
+        return node + node + 1;
+    }
+
+    int get_right_child(int node) {
+        return node + node + 2;
+    }
+
+    int get_parent(int node) {
+        return (node - 1) / 2;
+    }
+
 	void push(int data) {
 		heap.push_back(data);
-		int root = heap.size() - 1;
-		shift_up(root);
+		int node = heap.size() - 1;
+		shift_up(node);
 	}
 
-	void del() {
+	void pop() {
+        if (heap.empty()) {
+            cout << "ERROR" << endl;
+            return;
+        }
 		int last = heap.size() - 1;
-		int root = 0;
-		swap(heap[last], heap[root]);
+		int node = 0;
+		swap(heap[last], heap[node]);
 		heap.pop_back();
-		shift_down(root);
+		shift_down(node);
 	}
 
-	void shift_down(int root) {
-		if (root + root + 1 >= heap.size()) {
-			return;
-		}
-		int ch1 = root + root + 1;
-		int ch2 = root + root + 2;
-		int smallest = root;
+	void shift_down(int node) {
+		if (get_left_child(node) >= heap.size()) return;
+		int ch1 = get_left_child(node);
+		int ch2 = get_right_child(node);
+		int smallest = node;
 		if (heap[smallest] > heap[ch1]) {
 			smallest = ch1;
 		}
 		if (ch2 < heap.size() && heap[smallest] > heap[ch2]) {
 			smallest = ch2;
 		}
-		if (smallest != root) {
-			swap(heap[root], heap[smallest]);
+		if (smallest != node) {
+			swap(heap[node], heap[smallest]);
 			shift_down(smallest);
 		}
 	}
 
-	void shift_up(int root) {
-		if (root == 0) {
-			return;
-		}
-		int parent = (root - 1) / 2;
-		if (heap[parent] > heap[root]) {
-			swap(heap[parent], heap[root]);
+	void shift_up(int node) {
+		if (node == 0) return;
+		int parent = get_parent(node);
+		if (heap[parent] > heap[node]) {
+			swap(heap[parent], heap[node]);
 			shift_up(parent);
 		}
 	}
@@ -63,7 +75,7 @@ struct Heap {
 	void sort() {
 		while (!heap.empty()) {
 			cout << top() << " ";
-			del();
+			pop();
 		}
 		cout << endl;
 	}
@@ -75,14 +87,14 @@ int main()
 	vt<int> a(n);
 	For(i, n) a[i] = rand() % 100 + 1;
 	random_shuffle(a.begin(), a.end());
-    cout << "Random data: " << endl;
+    	cout << "Random data: " << endl;
 	For(i, n) cout << a[i] << " ";
 	cout << endl;
 	Heap hp = Heap();
 	For(i, n) {
 		hp.push(a[i]);
-    }
-    cout << "Sorted data: " << endl;
-    hp.sort();
+    	}
+    	cout << "Sorted data: " << endl;
+    	hp.sort();
 	return 0;
 }
